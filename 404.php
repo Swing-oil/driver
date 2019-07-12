e9e4bca8217ed88bab4894812e321f59842d2969
Swing-oil <44920669+Swing-oil@users.noreply.github.com>
2019-07-12 15:36:54 +0800

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,36 @@
+﻿<html>
+<head>
+<style type="text/css">
+.style1 {
+	text-align: center;
+}
+.style2 {
+	font-size: large;
+	font-family: "等线 Light";
+}
+.style3 {
+	text-align: center;
+	font-family: "微软雅黑 Light";
+	font-size: x-large;
+}
+a {
+	color: #000000;
+}
+a:visited {
+	color: #000000;
+}
+a:active {
+	color: #000000;
+}
+a:hover {
+	color: #000000;
+}
+</style>
+<title>404|没有东西呢┭┮﹏┭┮</title>
+</head>
+
+<p class="style1">&nbsp;</p>
+<p class="style1"><img alt="404" src="404.gif" width="215" height="200"><span class="style2">对不起对不起对不起！/(ㄒoㄒ)/~~</span></p>
+<p class="style3">这里没有你想要的东西。要不，5秒后回<a href="https://pigsier.github.io/">主页</a>？</p>
+<meta http-equiv="refresh" content="5;url=about:blank">
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Language" content="zh-cn" />
+<link rel="shortcut icon" href="/favicon.png" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>陈|私有盘不可侵犯</title>
+<style type="text/css">
+a {
+	color: #FFFFFF;
+}
+a:visited {
+	color: #FFFFFF;
+}
+a:active {
+	color: #FFFFFF;
+}
+a:hover {
+	color: #FFFFFF;
+}
+.style1 {
+	font-family: "微软雅黑 Light";
+	text-align: center;
+	font-size: 100pt;
+}
+.style2 {
+	text-align: center;
+	font-family: "等线 Light";
+	font-size: 54pt;
+}
+.style3 {
+	text-align: center;
+	font-family: "微软雅黑 Light";
+}
+</style>
+</head>
+
+<body style="color: #FFFFFF; background-image: url('/back.jpg'); background-color: #000000;">
+
+<p class="style1">陈:文件聚集地</p>
+<p class="style2"><a href="/Windows/index.php">Windows</a></p>
+<p class="style2"><a href="/Linux/index.php">Linux</a></p>
+<p class="style2"><a href="/Android/index.php">Android</a></p>
+<p class="style2"><a href="/Crx/index.php">Crx</a></p>
+<p class="style2"><a href="/SYNC列表/index.php">SYNC列表</a></p>
+<p class="style2"><a href="/网盘列表/index.php">网盘列表</a></p>
+<p class="style2"><a href="/激活码/index.php">激活码</a></p>
+<p class="style2"><a href="/BT/index.php">BT</a></p>
+<p class="style3">陈|Chan©2019-Forever</p>
+
+</body>
+
+</html>
