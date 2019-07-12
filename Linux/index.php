f298314cba116f9c22d48e277ada41f99b16e10a
Swing-oil <44920669+Swing-oil@users.noreply.github.com>
2019-07-12 15:47:39 +0800

    Add files via upload

--- Linux/index.php
+++ Linux/index.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /***************************************************************************
  *
  * 
@@ -11,7 +11,8 @@
  *
  * NB!:If you change anything, save with UTF-8! Otherwise you may
  *     encounter problems, especially when displaying images.
- *  汉化：Chen***************************************************************************/
+ *  汉化：Chen
+ ***************************************************************************/
 
 /***************************************************************************/
 /*   HERE ARE THE SETTINGS FOR CONFIGURATION                               */
@@ -90,7 +91,7 @@ $_CONFIG['show_top'] = true;
 // The title for the page
 // Default: $_CONFIG['main_title'] = "Encode Explorer";
 // 主标题
-$_CONFIG['main_title'] = "Pigsier的文件夹";
+$_CONFIG['main_title'] = "Chen的文件夹";
 
 //
 // The secondary page titles, randomly selected and displayed under the main header.
@@ -3129,7 +3130,7 @@ if($this->mobile == false && $this->getConfig("show_load_time") == true)
 	printf($this->getString("page_load_time")." | ", (microtime(TRUE) - $_START_TIME)*1000);
 }
 ?>
-<a href="/">Chen</a>
+<a href="">Chen</a>
 </div>
 <!-- END: Info area -->
 </body>
